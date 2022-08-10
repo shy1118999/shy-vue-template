c3f2684e0030b255f78a1b45a75011da3a631b8d
shy1118 <shy1118999@qq.com>
2022-08-10 19:10:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <main p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <router-view />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: shaohang-shy
+ * @Date: 2022-08-10 18:57:59
+ * @LastEditors: shaohang-shy
+ * @LastEditTime: 2022-08-10 19:00:03
+ * @Description: index page
+-->
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <p>Hello World!</p>
+  </div>
+</template>
