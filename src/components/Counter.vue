f26adb48df4dbfeffb7183cec569c4f90eafe244
shy1118 <shy1118999@qq.com>
2022-08-11 10:18:48 +0800

    feat: add test

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,7 +2,7 @@
  * @Author: shaohang-shy
  * @Date: 2022-08-11 09:14:44
  * @LastEditors: shaohang-shy
- * @LastEditTime: 2022-08-11 09:17:32
+ * @LastEditTime: 2022-08-11 10:15:51
  * @Description: Counter
 -->
 <script setup lang="ts">
@@ -18,10 +18,10 @@ const { count, inc, dec } = useCounter(props.initial)
       {{ count }}
     </div>
     <div>
-      <button class="btn m-2" @click="inc()">
+      <button class="inc btn m-2" @click="inc()">
         +
       </button>
-      <button class="btn m-2" @click="dec()">
+      <button class="dec btn m-2" @click="dec()">
         -
       </button>
     </div>
