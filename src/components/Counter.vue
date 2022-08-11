343d81e4d92e054a5864eb1382d8280855bd227b
shy1118 <shy1118999@qq.com>
2022-08-11 09:27:10 +0800

    feat:pages

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: shaohang-shy
+ * @Date: 2022-08-11 09:14:44
+ * @LastEditors: shaohang-shy
+ * @LastEditTime: 2022-08-11 09:17:32
+ * @Description: Counter
+-->
+<script setup lang="ts">
+const props = defineProps<{
+  initial: number
+}>()
+const { count, inc, dec } = useCounter(props.initial)
+</script>
+
+<template>
+  <div class="m-5">
+    <div>
+      {{ count }}
+    </div>
+    <div>
+      <button class="btn m-2" @click="inc()">
+        +
+      </button>
+      <button class="btn m-2" @click="dec()">
+        -
+      </button>
+    </div>
+  </div>
+</template>
